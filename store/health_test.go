177f23eb6f427d933fb5a197e3ee93f4a9461ab7
Ryan Huang <ryanhuang@cs.ucsd.edu>
2017-03-15 00:13:14 +0000

    rename test

--- store/health_test.go
+++ store/health_test.go
@@ -2,7 +2,7 @@ package store
 
 import "testing"
 
-func TestMakeHVector(t *testing.T) {
+func TestNewHVector(t *testing.T) {
 	var v *HVector
 	v = NewHVector("cpu", "disk", "network", "memory")
 	if v == nil {
