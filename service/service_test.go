1d9c0325b3e3c38b1414d85e7e9f0e6d82ccd571
Ryan Huang <ryanhuang@cs.ucsd.edu>
2017-03-17 06:49:15 +0000

    allow remote service test

--- service/service_test.go
+++ service/service_test.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
@@ -12,14 +13,17 @@ import (
 	"deephealth/store"
 )
 
-var r = rand.New(rand.NewSource(time.Now().UnixNano()))
-
 const (
 	portstart = 10000
 	portend   = 30000
 )
 
 var c *client.Client
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+var (
+	local = flag.Bool("local", true, "whether to perform localhost test only")
+	faddr = flag.String("addr", "localhost:30000", "use this address instead of localhost")
+)
 
 func BenchmarkAddReport(b *testing.B) {
 	o := dh.NewObservation(time.Now(), "cpu", "disk", "network")
@@ -38,12 +42,18 @@ func BenchmarkAddReport(b *testing.B) {
 }
 
 func TestMain(m *testing.M) {
-	port := portstart + int(r.Intn(portend-portstart))
-	addr := fmt.Sprintf("localhost:%d", port)
-	storage := store.NewRawHealthStorage("TS_1", "TS_2", "TS_3", "TS_4")
-	dh.SetLogLevel(dh.ErrorLevel)
-	hs := NewHealthService(addr, "XFE_1", storage)
-	hs.Start()
+	flag.Parse()
+	var addr string
+	if *local {
+		port := portstart + int(r.Intn(portend-portstart))
+		addr = fmt.Sprintf("localhost:%d", port)
+		storage := store.NewRawHealthStorage("TS_1", "TS_2", "TS_3", "TS_4")
+		dh.SetLogLevel(dh.ErrorLevel)
+		hs := NewHealthService(addr, "XFE_1", storage)
+		hs.Start()
+	} else {
+		addr = *faddr
+	}
 	c = client.NewClient(addr, false)
 	os.Exit(m.Run())
 }
