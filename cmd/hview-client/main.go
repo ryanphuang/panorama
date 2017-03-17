2999c9aea4ab95992d3286c0595aa077c2d036dc
Ryan Huang <ryanhuang@cs.ucsd.edu>
2017-03-17 08:33:17 +0000

    remove old ad-hoc client

--- cmd/hview-client/main.go
+++ cmd/hview-client/main.go
@@ -147,24 +147,4 @@ func main() {
 	} else {
 		runCmd(c, cmdArgs)
 	}
-
-	/*
-		o := dh.NewObservation(time.Now(), "cpu", "disk", "network")
-		o.SetMetric("cpu", dh.UNHEALTHY, 30)
-		o.SetMetric("disk", dh.HEALTHY, 90)
-		o.SetMetric("network", dh.HEALTHY, 95)
-		report := &dh.Report{
-			Observer:    "XFE_2",
-			Subject:     "TS_2",
-			Observation: *o,
-		}
-		var reply int
-		fmt.Printf("Calling add report to %s\n", addr)
-		err := c.AddReport(report, &reply)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Got reply %d\n", reply)
-		fmt.Println(report.Subject)
-	*/
 }
