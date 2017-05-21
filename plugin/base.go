77e9988172295e0080622f99dfe42cb9563f7885
Ryan Huang <ryanhuang@cs.ucsd.edu>
2017-05-21 05:15:31 +0000

    support plugin in logtail :)

---
+++ plugin/base.go
@@ -0,0 +1,14 @@
+package plugin
+
+import (
+	"flag"
+
+	dt "deephealth/types"
+)
+
+type LogTailPlugin interface {
+	ProvideFlags() *flag.FlagSet
+	ValidateFlags() error
+	Init() error
+	ProvideEventParser() dt.EventParser
+}
