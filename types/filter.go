c5d012abeb7404837f85cd0e1159b0280f7d74b9
Ryan Huang <ryanhuang@cs.ucsd.edu>
2017-05-25 10:57:30 +0000

    parse reg set

--- types/filter.go
+++ types/filter.go
@@ -84,6 +84,61 @@ func NewFieldEqualsFilter(field string, match string) FieldFilter {
 	}
 }
 
+func StringArrayToSlice(array string) ([]string, error) {
+	e := len(array) - 1
+	if array[0] != '[' || array[e] != ']' {
+		return nil, fmt.Errorf("Array must be enclosed in [ and ]")
+	}
+	parts := strings.Split(array[1:e], "',")
+	result := make([]string, len(parts))
+	for i, part := range parts {
+		part = strings.TrimLeft(part, " ")
+		if part[0] != '\'' {
+			return nil, fmt.Errorf("Element must start with ': %s", part)
+		}
+		if i == len(parts)-1 {
+			if part[len(part)-1] != '\'' {
+				return nil, fmt.Errorf("Element must end with': %s", part)
+			}
+			result[i] = part[1 : len(part)-1]
+		} else {
+			result[i] = part[1:]
+		}
+	}
+	return result, nil
+}
+
+func NewFieldRegexAnyFilter(field string, patterns ...string) FieldFilter {
+	res := make([]*regexp.Regexp, len(patterns))
+	for i, pattern := range patterns {
+		res[i] = regexp.MustCompile(pattern)
+	}
+	return func(fields map[string]string) (map[string]string, bool) {
+		for _, re := range res {
+			if re.MatchString(fields[field]) {
+				return nil, true
+			}
+		}
+		return nil, false
+	}
+}
+
+func NewFieldMRegexpMapAnyFilter(field string, prefix_group string, patterns ...string) FieldFilter {
+	res := make([]*du.MRegexp, len(patterns))
+	for i, pattern := range patterns {
+		res[i] = &du.MRegexp{regexp.MustCompile(pattern)}
+	}
+	return func(fields map[string]string) (map[string]string, bool) {
+		for _, re := range res {
+			match := re.FindStringSubmatchMap(fields[field], prefix_group)
+			if len(match) != 0 {
+				return match, true
+			}
+		}
+		return nil, false
+	}
+}
+
 func NewFieldRegexFilter(field string, pattern string) FieldFilter {
 	re := regexp.MustCompile(pattern)
 	return func(fields map[string]string) (map[string]string, bool) {
@@ -91,7 +146,7 @@ func NewFieldRegexFilter(field string, pattern string) FieldFilter {
 	}
 }
 
-func NewFieldMRegexpMapFilter(field string, pattern string, prefix_group string) FieldFilter {
+func NewFieldMRegexpMapFilter(field string, prefix_group string, pattern string) FieldFilter {
 	re := &du.MRegexp{regexp.MustCompile(pattern)}
 	return func(fields map[string]string) (map[string]string, bool) {
 		match := re.FindStringSubmatchMap(fields[field], prefix_group)
@@ -108,13 +163,27 @@ func NewFieldFilter(config *FieldFilterClauseConfig) (FieldFilter, error) {
 		filter = NewFieldNotEqualFilter(config.Field, config.Pattern)
 	case "~":
 		if config.CaptureResult {
-			filter = NewFieldMRegexpMapFilter(config.Field, config.Pattern, config.Field+"_")
+			filter = NewFieldMRegexpMapFilter(config.Field, config.Field+"_", config.Pattern)
 		} else {
 			filter = NewFieldRegexFilter(config.Field, config.Pattern)
 		}
 	case "(-":
-		elements := strings.Split(config.Pattern, ",")
+		elements, err := StringArrayToSlice(config.Pattern)
+		if err != nil {
+			return nil, err
+		}
 		filter = NewFieldInSetFilter(config.Field, elements...)
+	case "(~":
+		patterns, err := StringArrayToSlice(config.Pattern)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println(patterns)
+		if config.CaptureResult {
+			filter = NewFieldMRegexpMapAnyFilter(config.Field, config.Field+"_", patterns...)
+		} else {
+			filter = NewFieldRegexAnyFilter(config.Field, patterns...)
+		}
 	default:
 		return nil, fmt.Errorf("Unrecognized filter operator %s", config.Operator)
 	}
@@ -138,6 +207,7 @@ func NewFieldClassifier(config *ClassifierConfig) (FieldClassifier, error) {
 
 func NewFieldFilterTree(config *FieldFilterTreeConfig) (FieldFilterTree, error) {
 	var tree FieldFilterTree
+	fmt.Println("parsing")
 	for _, branch_config := range config.FilterTree {
 		head, err := NewFieldFilter(branch_config.Head)
 		if err != nil {
