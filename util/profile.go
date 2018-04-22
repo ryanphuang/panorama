3aada4aa9e2ed1f1f0e25c117b68fdb99099e835
Ryan Huang <huang@cs.jhu.edu>
2018-04-22 14:02:41 -0600

    print utc timestamp

--- util/profile.go
+++ util/profile.go
@@ -13,7 +13,7 @@ func bToMb(b uint64) float64 {
 
 func PrintMemUsage(w io.Writer) {
 	var m runtime.MemStats
-	now := time.Now()
+	now := time.Now().UTC().Format("2006-01-02T15:04:05.999Z")
 	runtime.ReadMemStats(&m)
-	fmt.Fprintf(w, "%s,%.4f,%.4f,%.4f,%d\n", now.Format(time.RFC3339Nano), bToMb(m.Alloc), bToMb(m.TotalAlloc), bToMb(m.Sys), m.NumGC)
+	fmt.Fprintf(w, "%s,%.4f,%.4f,%.4f,%d\n", now, bToMb(m.Alloc), bToMb(m.TotalAlloc), bToMb(m.Sys), m.NumGC)
 }
